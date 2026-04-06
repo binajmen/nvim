6614c469966ccde0379a7438118ac249c9d66294
Benjamin Decoster <bdcstr@gmail.com>
2026-04-06 10:26:22 +0200

    wip

--- lua/custom/plugins/review.lua
+++ lua/custom/plugins/review.lua
@@ -1,8 +1,24 @@
 return {
-  dir = vim.fn.stdpath('config') .. '/lua/review-comments',
-  name = 'review-comments',
-  event = 'VeryLazy',
-  config = function()
-    require('review-comments').setup()
-  end,
+  'binajmen/review.nvim',
+  lazy = false,
+  opts = {
+    signs = {
+      enabled = true,
+      text = 'R',
+      hl = 'DiagnosticInfo',
+      priority = 5,
+    },
+    virtual_lines = {
+      enabled = true,
+      prefix = '💬 ',
+      hl = 'DiagnosticInfo',
+    },
+  },
+  keys = {
+    { '<leader>ra', '<Plug>(ReviewAdd)', mode = 'v', desc = 'Review: add comment on selection' },
+    { '<leader>ra', '<Plug>(ReviewAdd)', mode = 'n', desc = 'Review: add file comment' },
+    { '<leader>rl', '<Plug>(ReviewList)', desc = 'Review: list comments' },
+    { '<leader>ry', '<Plug>(ReviewYank)', desc = 'Review: yank comments' },
+    { '<leader>rc', '<Plug>(ReviewClear)', desc = 'Review: clear comments' },
+  },
 }
